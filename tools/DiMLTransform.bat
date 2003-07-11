79f51937dd2e1e3e683ee1f0da32d6637bed676c
Jakob Voss <jakob@nichtich.de>
2003-07-11 12:14:06 +0000

    restore DiMLTransform.bat

--- tools/DiMLTransform.bat
+++ tools/DiMLTransform.bat
@@ -1,7 +1,50 @@
 @echo off
+rem Guess DIMLXSL if not defined
+if not "%DIMLXSL%" == "" goto gotHome
+echo DIMLXSL not set - using ..
+set DIMLXSL=..
 
-rem java -DDIMLXSL=.. -classpath "../tools/;../lib/xml-apis.jar;../lib/xalan.jar;../lib/xercesImpl.jar" DiMLTransform %1 %2
-rem java -DDIMLXSL="D:\projects\diml\diml-xsl with cvs\diml-xsl" -classpath "D:\projects\diml\diml-xsl with cvs\diml-xsl/tools/;D:\projects\diml\diml-xsl with cvs\diml-xsl/lib/xml-apis.jar;D:\projects\diml\diml-xsl with cvs\diml-xsl/lib/xercesImpl.jar" "D:\projects\diml\diml-xsl with cvs\diml-xsl\tools\DiMLTransform" %1 %2
-java "D:/projects/diml/diml-xsl with cvs/diml-xsl/tools/DiMLTransform"
+:gotHome
+if exist "%DIMLXSL%\tools\DiMLTransform.bat" goto okHome
+echo The DIMLXSL environment variable is not defined correctly
+echo This environment variable is needed to run this program
+goto end
 
+:okHome
+rem Get standard Java environment variables
+if exist "%DIMLXSL%\tools\setclasspath.bat" goto okSetclasspath
+echo Cannot find %DIMLXSL%\tools\setclasspath.bat
+echo This file is needed to run this program
+goto end
 
+:okSetclasspath
+REM echo "%_RUNJAVA%"
+REM if exist "%_RUNJAVA%" goto okJava
+REM echo Please set JAVA_HOME to point to your installation of Java
+REM goto end
+
+:okJava
+rem Do you have Xalan?
+if not exist "%DIMLXSL%\lib\xml-apis.jar" goto noXalan
+if not exist "%DIMLXSL%\lib\xalan.jar" goto noXalan
+if not exist "%DIMLXSL%\lib\xercesImpl.jar" goto noXalan
+goto okXalan
+
+:noXalan
+echo Xalan or Xerces jar file is missing.
+echo Please put the file xml-apis.jar, xalan.jar and xercesImpl.jar
+echo into the directory %DIMLXSL%\lib
+goto end
+
+:okXalan
+set BASEDIR=%DIMLXSL%
+call "%DIMLXSL%\tools\setclasspath.bat"
+set CLASSPATH=%CLASSPATH%;%DIMLXSL%\lib\xml-apis.jar;%DIMLXSL%\xalan.jar;%DIMLXSL%\lib\xercesImpl.jar;%DIMLXSL%\tools
+
+:exec
+set MAINCLASS=DiMLTransform
+set ARGUMENTS=%*
+REM @echo on
+%_RUNJAVA% -DDIMLXSL=%DIMLXSL% -classpath "%CLASSPATH%" %MAINCLASS% %ARGUMENTS%
+
+:end

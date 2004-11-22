db0cab45c324c0e047deb78038537c2ce6ab8e89
archibaldsmith <archibaldsmith>
2004-11-22 11:41:48 +0000

    variables for javaw.exe, jdb.exe and javac.exe not any more mandatory

--- tools/setclasspath.bat
+++ tools/setclasspath.bat
@@ -9,9 +9,9 @@ echo Try setting this variable if DiMLTranform fails
 goto end
 :gotJavaHome
 if not exist "%JAVA_HOME%\bin\java.exe" goto noJavaHome
-if not exist "%JAVA_HOME%\bin\javaw.exe" goto noJavaHome
-if not exist "%JAVA_HOME%\bin\jdb.exe" goto noJavaHome
-if not exist "%JAVA_HOME%\bin\javac.exe" goto noJavaHome
+REM if not exist "%JAVA_HOME%\bin\javaw.exe" goto noJavaHome
+REM if not exist "%JAVA_HOME%\bin\jdb.exe" goto noJavaHome
+REM if not exist "%JAVA_HOME%\bin\javac.exe" goto noJavaHome
 goto okJavaHome
 :noJavaHome
 echo Error: The JAVA_HOME environment variable is not defined correctly
@@ -22,14 +22,14 @@ goto end
 rem Set standard CLASSPATH
 rem Note that there are no quotes as we do not want to introduce random
 rem quotes into the CLASSPATH
-set CLASSPATH=%JAVA_HOME%\lib\tools.jar
+REM set CLASSPATH=%JAVA_HOME%\lib\tools.jar
 
 rem Set standard command for invoking Java.
 rem Note that NT requires a window name argument when using start.
 rem Also note the quoting as JAVA_HOME may contain spaces.
 set _RUNJAVA="%JAVA_HOME%\bin\java"
-set _RUNJAVAW="%JAVA_HOME%\bin\javaw"
-set _RUNJDB="%JAVA_HOME%\bin\jdb"
-set _RUNJAVAC="%JAVA_HOME%\bin\javac"
+REM set _RUNJAVAW="%JAVA_HOME%\bin\javaw"
+REM set _RUNJDB="%JAVA_HOME%\bin\jdb"
+REM set _RUNJAVAC="%JAVA_HOME%\bin\javac"
 
 :end
